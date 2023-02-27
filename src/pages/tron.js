d1ba78aa15053015c6713f28fcb8f64c276d3718
mechaneyes <ray@mechaneyes.com>
2023-02-27 06:45:37 -0800

    slight refactoring + light styling
    + tweaked useInterval() killing the console.log which was throwing an
    error when images would loop
    + tweaked timings
    + centered images on page, requiring new div w classes

--- src/pages/tron.js
+++ src/pages/tron.js
@@ -5,16 +5,15 @@ import "./tron.scss";
 
 export default function Looper() {
   const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
   const [currentImage, setCurrentImage] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
   const [fadeIn, setFadeIn] = useState(true);
 
   // Making setInterval Declarative with React Hooks
   // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-  // 
+  //
   // Really gottta figure out how this thing works.
-  // 
+  //
   function useInterval(callback, delay) {
     const savedCallback = useRef();
 
@@ -38,26 +37,23 @@ export default function Looper() {
 
   useEffect(() => {
     setData(null);
-    setLoading(true);
     fetch("/api/tron-api")
       .then((res) => res.json())
       .then((data) => {
         console.log("data", data);
         setData(data);
-        setLoading(false);
         setCurrentImage(data.tronArray[0].file);
       });
   }, []);
 
-  // if (isLoading) return <p>Loading...</p>;
-  // if (!data) return <p>No profile data</p>;
-
   useInterval(() => {
-    if (currentIndex === data.tronArray.length) {
-      setCurrentIndex(0);
+    if (currentIndex >= data.tronArray.length) {
+      setCurrentImage(data.tronArray[0].file);
+      setCurrentIndex(1);
+    } else {
+      setCurrentImage(data.tronArray[currentIndex].file);
+      setCurrentIndex(currentIndex + 1);
     }
-    setCurrentImage(data.tronArray[currentIndex].file);
-    setCurrentIndex(currentIndex + 1);
 
     setTimeout(() => {
       setFadeIn(true);
@@ -65,23 +61,25 @@ export default function Looper() {
 
     setTimeout(() => {
       setFadeIn(false);
-    }, 7000);
+    }, 9000);
 
-    console.log("currentImage", data.tronArray[currentIndex].file);
-  }, 8000);
+    // console.log("currentImage", data.tronArray[currentIndex].file);
+  }, 10000);
 
   return (
     <>
-      <div className="img-container">
-        <Image
-          className={`tron-image ${
-            fadeIn ? "tron-image--fade-in" : "tron-image--fade-out"
-          }`}
-          src={`/tron/${currentImage}`}
-          alt="Still image from the movie Tron. Sorry this isn't more descriptive. The images are setup programatically and I'm not sure how to get a good description to display here."
-          fill
-          sizes="100vw"
-        />
+      <div className="page page--tron">
+        <div className="img-container">
+          <Image
+            className={`tron-image ${
+              fadeIn ? "tron-image--fade-in" : "tron-image--fade-out"
+            }`}
+            src={`/tron/${currentImage}`}
+            alt="Still image from the movie Tron. Sorry this isn't more descriptive. The images are setup programatically and I'm not sure how to get a good description to display here."
+            fill
+            sizes="100vw"
+          />
+        </div>
       </div>
     </>
   );
