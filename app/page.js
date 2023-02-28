f1f0853a67338217a942656992e9a229685daaa0
mechaneyes <ray@mechaneyes.com>
2023-02-28 07:07:25 -0800

    tweaks to homepage
    + setup next/link to link to /tron page
    + commented out legacy code
    + removed fadeIn. was causing build error

--- app/page.js
+++ app/page.js
@@ -1,62 +1,54 @@
-import fs from "fs";
+// import fs from "fs";
 import Image from "next/image";
+import Link from "next/link";
 
-let tronArray = [];
+// let tronArray = [];
 
-const TronImages = () => {
-  // let tronArray = [];
+// const TronImages = () => {
+//   // let tronArray = [];
 
-  fs.readdir("./app/tron-images/", (err, files) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+//   fs.readdir("./app/tron-images/", (err, files) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
 
-    // console.log("files", files);
+//     // console.log("files", files);
 
-    tronArray = files;
-    // console.log('tronArray', tronArray)
-    return tronArray;
+//     tronArray = files;
+//     // console.log('tronArray', tronArray)
+//     return tronArray;
+//   });
 
-    // files.map((file) => {
-    //   let tronUrl = `tron-images/${file}`;
-    //   // console.log("tronUrl", tronUrl);
-    //   tronArray.push({ tronUrl });
-    // });
-  });
+//   return <img src={`/tron-images/${tronArray[0]}`} />;
 
-  return <img src={`/tron-images/${tronArray[0]}`} />;
-  return console.log("tronArray outside", tronArray[0]);
-
-  return tronArray.map((tron) => {
-    // return <p>{tron}</p>
-    return (
-      <img
-        src={`/tron-images/${tron}`}
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
-    );
-  });
-};
+//   return tronArray.map((tron) => {
+//     // return <p>{tron}</p>
+//     return (
+//       <img
+//         src={`/tron-images/${tron}`}
+//         alt="Picture of the author"
+//         width={500}
+//         height={500}
+//       />
+//     );
+//   });
+// };
 
 export default function Page() {
   return (
     <>
-      <h1>
-        <a href="/tron">Tron</a>
-      </h1>
+      <Link href="/tron">
+        <h1>Tron</h1>
 
-      <Image
-        className={`tron-image ${
-          fadeIn ? "tron-image--fade-in" : "tron-image--fade-out"
-        }`}
-        src={`/tron/${currentImage}`}
-        alt="Still image from the movie Tron. Sorry this isn't more descriptive. The images are setup programatically and I'm not sure how to get a good description to display here."
-        fill
-        sizes="100vw"
-      />
+        <Image
+          className="tron-image"
+          src="/tron/tron-stills_055.jpg"
+          alt="Still image from the movie Tron. Sorry this isn't more descriptive. The images are setup programatically and I'm not sure how to get a good description to display here."
+          fill
+          sizes="100vw"
+        />
+      </Link>
     </>
   );
 }
