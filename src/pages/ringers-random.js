7511386e029d4a95c01a021e75a04f0bf1552bb3
mechaneyes <ray@mechaneyes.com>
2023-03-05 06:29:12 -0800

    dropped css variable config from ringers-random.js
    + turns out the sass conditionals do all of the heavy lifting
    + so adjusting the css variables in js isn't necessary
    + just did RingersRando. need to clean up other pages

--- src/pages/ringers-random.js
+++ src/pages/ringers-random.js
@@ -1,22 +1,7 @@
-import { useEffect } from "react";
-
 import RingersRandomLone from "./ringers-random-lone";
 import "./ranger.scss";
 
 export default function RingersRando() {
-  useEffect(() => {
-    const root = document.documentElement;
-    if (window.innerWidth > window.innerHeight) {
-      root.style.setProperty("--flex-direct", "row");
-      root.style.setProperty("--img-width", "auto");
-      root.style.setProperty("--img-height", "100%");
-    } else {
-      root.style.setProperty("--grid-col", "1fr");
-      root.style.setProperty("--img-width", "100%");
-      root.style.setProperty("--img-height", "auto");
-    }
-  }, []);
-
   return (
     <>
       <div className="page page--square--double">
